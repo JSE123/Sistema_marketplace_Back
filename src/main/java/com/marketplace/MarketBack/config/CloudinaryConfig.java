c50db3f7e84e3d1967ad9646894c8ca09700a4e3
JSE123 <jsemnuel101@gmail.com>
2025-07-31 15:01:05 -0600

    Actualizacion de variables de entorno

--- src/main/java/com/marketplace/MarketBack/config/CloudinaryConfig.java
+++ src/main/java/com/marketplace/MarketBack/config/CloudinaryConfig.java
@@ -3,6 +3,7 @@ package com.marketplace.MarketBack.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,23 +11,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@ConfigurationProperties(prefix = "cloudinary")
 public class CloudinaryConfig {
-    @Bean
-    public Cloudinary cloudinary(){
-        final Map<String, String> Config = new HashMap<>();
-        Config.put("cloud_name", "dayppnink");
-        Config.put("api_key", "658811558468439");
-        Config.put("api_secret", "uWYaTUS-2wbbk1qNj4Vr20np1F4");
-        return new Cloudinary(Config);
+
+
+    private final CloudinaryProperties properties;
+
+    public CloudinaryConfig(CloudinaryProperties properties) {
+        this.properties = properties;
     }
 
-//    @Bean
-//    public Cloudinary cloudinary() {
-//        return new Cloudinary(ObjectUtils.asMap(
-//                "cloud_name", cloudName,
-//                "api_key", apiKey,
-//                "api_secret", apiSecret,
-//                "secure", true
-//        ));
-//    }
+    @Bean
+    public Cloudinary cloudinary() {
+        final Map<String, String> Config = new HashMap<>();
+        Config.put("cloud_name", this.properties.getCloudName());
+        Config.put("api_key", this.properties.getApiKey());
+        Config.put("api_secret", this.properties.getApiSecret());
+        return new Cloudinary(Config);
+    }
+//Config.put("cloud_name", "dayppnink");
+//        Config.put("api_key", "658811558468439");
+//        Config.put("api_secret", "uWYaTUS-2wbbk1
 }
